8c1f1c15ca9f2f824e0d4473ba3972cbb1ada737
Reda-BENMAKDAD <redabenmakdad@gmail.com>
2023-03-30 11:10:35 +0000

    improved logic and code readabilty

--- classes.js
+++ classes.js
@@ -7,11 +7,25 @@ class Piece {
     }
 
 
+
 }
 
 class King extends Piece {
     constructor (color){
         super("king", color);
+    }
+    canMove (origin, destination) {
+    console.log("called can move from king");
+    return  ((origin.x === destination.x  && origin.y === destination.y +1) || 
+            (origin.x === destination.x  && origin.y === destination.y -1) ||
+            (origin.x === destination.x +1 && origin.y === destination.y) ||
+            (origin.x === destination.x -1 && origin.y === destination.y) ||
+            (origin.x === destination.x +1 && origin.y === destination.y +1) ||
+            (origin.x === destination.x +1 && origin.y === destination.y -1) ||
+            (origin.x === destination.x -1 && origin.y === destination.y +1) ||
+            (origin.x === destination.x -1 && origin.y === destination.y -1) ||
+            ((game[destination.x][destination.y].color !== this.color)))
+            
     }
 
 }
@@ -20,6 +34,9 @@ class Queen extends Piece {
     constructor (color) {
         super("queen", color);
     }
+    canMove (origin, destination) {
+        return true;
+    }
 
 }
 
@@ -27,11 +44,17 @@ class Bishop extends Piece {
     constructor (color) {
         super("bishop", color);
     }
+    canMove (origin, destination) {
+        return true;
+    }
 }
 
 class Knight extends Piece {
     constructor (color) {
         super("knight", color);
+    }
+    canMove (origin, destination) {
+        return true;
     }
 }
 
@@ -39,12 +62,20 @@ class Rook extends Piece {
     constructor (color) {
         super("rook", color);
     }
+    canMove (origin, destination) {
+        return true;
+    }
 }
 
 class Pawn extends Piece {
     constructor (color) {
         super("pawn", color);
     }
+
+    canMove (origin, destination) {
+        return true;
+    }
+
 
 }
 
--- logic.js
+++ logic.js
@@ -1,43 +1,50 @@
 
-
-let choosenCell = null;
-let destinationCell = null;
+let origin = { x: null, y: null };
+let destination = { x:null , y: null };
 
 // this adds an event listener to each cell
 // so that each time a cell is clicked
 // it will be determined if it is the first click or the second click
-// if it's first click then the choosenCell will be the piece that was clicked and it will be recorded
-// it it's second click then destinationCell will be the cell we want to move to
-// but before moving it will be checked if it's legal to move to that cell
+// and do the appropriate action
+
 Array.from(document.getElementsByClassName("cell")).forEach((cell) => {
-    cell.addEventListener("click", (e) => {
-    
-        // determine if it's the first click or the second click
-        if (choosenCell === null) {
-            choosenCell = e.target.id.split("-").map((num) => parseInt(num));
-        } else {
-            // in this case it's the second click
-            // so we need to check if moving to this destination cell is legal
-            destinationCell = e.target.id.split("-").map((num) => parseInt(num));
+  cell.addEventListener("click", (e) => {
 
-            if (
-                // this cheks if the choosenCell was empty
-                // if it is then we don't need to do anything
-                !(game[choosenCell[0]][choosenCell[1]] === "") && 
+    // in this case it's the first click (to choose the piece we want to move)
+    // we know it because origin coordinates are null
+    if (origin.x === null && origin.y === null) {
 
-                // this checks if the user clicked on the same cell for choosenCell and destinationCell
-                !(choosenCell[0] === destinationCell[0] && choosenCell[1] === destinationCell[1])
+      // we now set the coordinates of the cell we clicked on
+      origin.x = e.target.id.split("-").map((num) => parseInt(num))[0];
+      origin.y = e.target.id.split("-").map((num) => parseInt(num))[1];
 
-                
-            ) {
-                game[destinationCell[0]][destinationCell[1]] = game[choosenCell[0]][choosenCell[1]];
-                game[choosenCell[0]][choosenCell[1]] = "";
-                choosenCell = null;
-                destinationCell = null;
-                renderBoard();
+    } else {
 
-            }
-        }
-            
-    });
-})
+      // in this case it's the second click
+      // because origin coordinates are already filled
+      // we now set the coordinates of the cell we want to move to
+      destination.x = e.target.id.split("-").map((num) => parseInt(num))[0];
+      destination.y = e.target.id.split("-").map((num) => parseInt(num))[1];
+      console.log(game[origin.x][origin.y]);
+      
+      // here we check if the move is legal
+      if (
+        // this cheks if the choosen cell was empty
+        // if it is, then we don't need to do anything
+        !(game[origin.x][origin.y] === "") &&
+
+        // this checks if the user clicked on the same cell two times
+
+        (game[origin.x][origin.y].canMove(origin, destination))
+      ) {
+        game[destination.x][destination.y] = game[origin.x][origin.y];
+        game[origin.x][origin.y] = "";
+        origin = { x: null, y: null}
+        destination = { x: null, y: null}
+        renderBoard();
+      } else {
+        destination = { x: null, y: null}
+      }
+    }
+  });
+});
